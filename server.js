0c9b3407363a77992f50299b9a05ec16466b9dcf
Devin Gibson <dgib21@hotmail.com>
2020-10-11 10:15:22 -0500

    changingPorttoVariable

---
+++ server.js
@@ -0,0 +1,24 @@
+//everything that will be required 
+
+const express = require('express');
+const path = require("path");
+const fs = require("fs");
+
+//intializing express server 
+const app = express();
+
+//declaring which port we will listen on 
+const PORT = process.env.PORT || 3001;
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+//makes css files availbale to the server 
+app.use(express.static('public'));
+
+
+// code to have the application listening for request 
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
