0f6399a8ed6c0703a36c7ef330c0cf72a833eae3
Devin Gibson <dgib21@hotmail.com>
2020-10-11 10:32:30 -0500

    routes folder

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+//   //displays our home page(index.html)
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/assets/index.html'));
+//   });
+// //displays the animals page 
+//   app.get('/animals', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/assets/animals.html'));
+//   });
+// //displays the animals page 
+//   app.get('/zookeepers', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/assets/zookeepers.html'));
+//   });
+
+//requiring path 
+const path = require("path");
+//initializing router with express
+const router = require("express").Router();
+
+//home page
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+  });
+
+//send the html pages to render the pages
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/notes.html"));
+});
+
+// if we don't have a URL send them to our index html page 
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+module.exports = router;
