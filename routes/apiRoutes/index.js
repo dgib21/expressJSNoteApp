db7b219d219dfb5f011be1ed9e683688584d4166
Devin Gibson <dgib21@hotmail.com>
2020-10-12 14:43:04 -0500

    added api routes to api route folder

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,35 +1,53 @@
 
-// requires the DB file 
+
 const { db } = require("../../db/db.json");
-const { get } = require("../html");
-
-//initializing router with express
 const router = require("express").Router();
+const fs = require("fs");
+const store = require("../../db/store.js");
+const { getNotes } = require("../../db/store.js");
+const uuidv1 = require("uuidv1"); 
 
 
- const store = require("../../db/store.js");
- // const { getNotes } = require("../../db/store.js");
+//setting variable to read old notes from db
+const savedNotes = fs.readFileSync("./db/db.json", "UTF-8");
+// if we have notes in the db set them to a variable 
+if (savedNotes) {
+  var dbNotes = JSON.parse(savedNotes);
+  notes = dbNotes;
+} else {
+  notes = [];
+}
 
 
-
-router.get('/notes', (req, res) => {
-    
-  const allNotes = store.getNotes(); 
-  console.log(allNotes); 
-  res.send("all notes");  
+//display notes on page
+router.get("/notes", (req, res) => {
+  return res.json(notes);
 });
 
-// /notes/{id}
+//collect client input data, store it and write it to the page
+router.post("/notes", function (req, res) {
+ 
+  //assign a random ID
+  let noteId = uuidv1();
+  //structure the note object
+   let newNote = {
+    id: noteId,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  
+  notes.push(newNote);
 
+  res.json(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), function (
+    err
+  ) {
+    if (err) throw err;
+  });
+});
 
 
 module.exports = router;
 
-// display notes on page 
 
-//add note route 
-
-// delete note route 
-
-//get notes route 
  
